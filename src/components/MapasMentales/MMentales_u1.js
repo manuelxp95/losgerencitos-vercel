500ab7592979a0c4ee81033bcd3748c7091695bc
manuelxp95 <80785834+manuelxp95@users.noreply.github.com>
2024-04-22 21:43:29 -0300

    zoom para la mapmental

--- src/components/MapasMentales/MMentales_u1.js
+++ src/components/MapasMentales/MMentales_u1.js
@@ -1,14 +1,18 @@
+import React, { useState } from 'react';
 import Logo from "../Logo/Logo";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import meliMcGonagall from '../../images/miembro1.jpeg';
-import mapmental1 from '../../images/MentalUnidad1.png';
-
 import { useNavigate } from "react-router-dom";
+import mapmental1 from '../../images/MentalUnidad1.png';
 
 const MMentalesDet = () => {
     const navigate = useNavigate();
+    const [fullscreen, setFullscreen] = useState(false);
+
+    const toggleFullscreen = () => {
+        setFullscreen(!fullscreen);
+    };
 
     return (
         <Container fluid className="main-container">
@@ -16,14 +20,23 @@ const MMentalesDet = () => {
                 <Logo />
             </Row>
             <Row className="justify-content-center mt-3 pb-3">
-            <div className="image-container text-center">
-            <img
-                src= {mapmental1}
-                alt="Imagen desde Google Drive"
-                width="90%"
-                height="auto"
-            />
-            </div>
+                <div className="image-container text-center">
+                    {fullscreen && (
+                        <div className="fullscreen-overlay" onClick={toggleFullscreen}>
+                            <img src={mapmental1} alt="Mapa Mental Unidad 1" />
+                        </div>
+                    )}
+                    {!fullscreen && (
+                        <img
+                            src={mapmental1}
+                            alt="Mapa Mental Unidad 1"
+                            width="90%"
+                            height="auto"
+                            onClick={toggleFullscreen}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    )}
+                </div>
             </Row>
             <div className="text-center mt-3 pb-3">
                 <Button variant="btn btn-primary" onClick={() => navigate("/mapmentales")}>Back</Button>
