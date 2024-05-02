a4d673aeae395ae27d1d4bd655abff82d5123000
manuelxp95 <manuelxp95@gmail.com>
2024-05-02 15:06:06 -0300

    -titulos

--- src/components/MapasMentales/MMentales_u1.js
+++ src/components/MapasMentales/MMentales_u1.js
@@ -11,7 +11,7 @@ import mapmentalmelina from '../../images/MapaMelina.webp';
 
 const MMentalesDet = () => {
     const navigate = useNavigate();
-    const [fullscreen, setFullscreen] = useState({ map1: false, map2: false, map3:false });
+    const [fullscreen, setFullscreen] = useState({ map1: false, map2: false, map3: false });
 
     const toggleFullscreen = (map) => {
         setFullscreen({ ...fullscreen, [map]: !fullscreen[map] });
@@ -21,6 +21,11 @@ const MMentalesDet = () => {
         <Container fluid className="main-container">
             <Row className="group-logo justify-content-center mt-3 pb-3">
                 <Logo />
+            </Row>
+
+            {/* Título para la primera imagen */}
+            <Row className="justify-content-center mt-3 pb-3">
+                <h3 style={{ color: 'white' }}>Mapa Manuel Ortiz</h3>
             </Row>
             <Row className="justify-content-center mt-3 pb-3">
                 <div className="image-container text-center">
@@ -41,17 +46,22 @@ const MMentalesDet = () => {
                     )}
                 </div>
             </Row>
+
+            {/* Título para la segunda imagen */}
+            <Row className="justify-content-center mt-3 pb-3">
+                <h3 style={{ color: 'white' }}>Mapa Christian Ramirez</h3>
+            </Row>
             <Row className="justify-content-center mt-3 pb-3">
                 <div className="image-container text-center">
                     {fullscreen.map2 && (
                         <div className="fullscreen-overlay" onClick={() => toggleFullscreen('map2')}>
-                            <img src={mapmentalramirez} alt="Mapa Mental Unidad 1" />
+                            <img src={mapmentalramirez} alt="Mapa Mental Ramirez" />
                         </div>
                     )}
                     {!fullscreen.map2 && (
                         <img
                             src={mapmentalramirez}
-                            alt="Mapa Mental Unidad 1"
+                            alt="Mapa Mental Ramirez"
                             width="90%"
                             height="auto"
                             onClick={() => toggleFullscreen('map2')}
@@ -60,17 +70,22 @@ const MMentalesDet = () => {
                     )}
                 </div>
             </Row>
+
+            {/* Título para la tercera imagen */}
+            <Row className="justify-content-center mt-3 pb-3">
+                <h3 style={{ color: 'white' }}>Mapa Melina Teruel Alonso</h3>
+            </Row>
             <Row className="justify-content-center mt-3 pb-3">
                 <div className="image-container text-center">
                     {fullscreen.map3 && (
                         <div className="fullscreen-overlay" onClick={() => toggleFullscreen('map3')}>
-                            <img src={mapmentalmelina} alt="Mapa Mental Unidad 1" />
+                            <img src={mapmentalmelina} alt="Mapa Mental Melina" />
                         </div>
                     )}
                     {!fullscreen.map3 && (
                         <img
                             src={mapmentalmelina}
-                            alt="Mapa Mental Unidad 1"
+                            alt="Mapa Mental Melina"
                             width="90%"
                             height="auto"
                             onClick={() => toggleFullscreen('map3')}
@@ -79,6 +94,7 @@ const MMentalesDet = () => {
                     )}
                 </div>
             </Row>
+
             <div className="text-center mt-3 pb-3">
                 <Button variant="btn btn-primary" onClick={() => navigate("/mapmentales")}>Back</Button>
             </div>
